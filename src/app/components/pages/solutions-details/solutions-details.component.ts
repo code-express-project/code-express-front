ad349c8eedcd69c6917386e0cb3ba8752c774d92
Danilo <danilofrancisquete@hotmail.com>
2024-04-12 17:10:07 -0300

    website rebilling

--- src/app/components/pages/solutions-details/solutions-details.component.ts
+++ src/app/components/pages/solutions-details/solutions-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-solutions-details',
@@ -11,5 +12,38 @@ export class SolutionsDetailsComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  clientWra: OwlOptions = {
+		loop:true,
+		margin:30,
+		nav:false,
+		mouseDrag: true,
+		items:1,
+		dots: false,
+		autoHeight: true,
+		autoplay: true,
+		smartSpeed: 800,
+		autoplayHoverPause: true,
+		center: false,
+		responsive:{
+			0:{
+				items:1,
+				margin: 10,
+			},
+			576:{
+				items:1,
+			},
+			768:{
+				items:2,
+				margin: 20,
+			},
+			992:{
+				items:2,
+			},
+			1200:{
+				items:2,
+			}
+		}
+    }
 
 }
